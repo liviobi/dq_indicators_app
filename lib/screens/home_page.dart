a7a38ab318e2def708b71e3ae9324af68dbdcbce
liviobi <10546479@polimi.it>
2022-06-29 19:06:11 +0200

    remove comments

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -14,26 +14,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: SingleChildScrollView(
         child: Center(
           child: Column(
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               const FileUploadWithHttp(),
